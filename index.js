fc0bbe9fd5609c84cfec4fc5cb08179274fe5d99
Elvina Nazipova <elvina.valeeva.2002@bk.ru>
2024-02-17 00:21:11 +0500

    1work на JS

--- index.js
+++ index.js
@@ -11,3 +11,16 @@ if (((visa && money) || ticket === international || !emotionnot) && (age>  18)) 
 } else {
     console.log("Постарайтесь в следующий раз");
 }
+
+
+const weather = "sunny"
+const time = "night"
+
+if ("rain" === weather) {
+    console.log("Еду на автобусе");
+} else if (time === "morning") {
+    console.log('Еду на велике');
+}
+else {
+    console.log("Еду на машине");
+}
