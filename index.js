c0e0328d0c075f1e09d13c3c03443a6165cd3176
Elvina Nazipova <elvina.valeeva.2002@bk.ru>
2024-02-23 23:49:47 +0500

    Выполнила все 5 заданий по JS

--- index.js
+++ index.js
@@ -1,85 +1,68 @@
-//Задание 1. 
-//Напишите функцию multiple(a, b), которая перемножает два числа между собой. Функция возвращает результат умножения.
-
-console.log(multiple(1, 5));
-function multiple(a, b) {
-    return a * b;
+//1
+console.log(multiple(5, 7));
+function multiple (a, b) {
+    return a*b;
 }
 
-//Задание 2 
-//Напишите функцию buildDate(day, month, year), которая принимает на вход числами день недели, месяц и год. Функция возвращает дату строкой в формате dd/mm/yyyy.
-
- 
-const date = buildDate(20, 2, 2024)
-
-console.log(date);
-
-function buildDate(day, month, year) {
-   return `${day.toString()}/${month.toString().padStart(2, '0')}/${year.toString()}`
+//2
+console.log(buildDate(23,2,2024));
+function buildDate(day,month,year) {
+    return `${day}/${month.toString().padStart(2, '0')}/${year}`
 }
 
 //3
 console.log(isAdult(21));
 function isAdult(age) {
-  
-    return age > 18 || age == 18;
-    
+    return age >=18;
 }
 
 //4
-
-console.log(getMonthByNumber(2));
-
-
-function getMonthByNumber (monthNumber) {
-    switch (monthNumber) {
+console.log(getMonthByNumber(10));
+function getMonthByNumber(monthNunmer) {
+    switch (monthNunmer) {
         case 1:
-            return 'Январь';
-        case 2:
-            return 'Февраль';
-        case 3:
-            return 'Март';
-        case 4:
-            return 'Апрель';
-        case 5:
-            return 'Май';
-        case 6:
-            return 'Июнь';
-        case 7:
-            return 'Июль';
-        case 8:
-            return 'Август';
-        case 9:
-            return 'Сентябрь';
-        case 10:
-            return 'Октябрь';
-        case 11:
-            return 'Ноябрь';
-        case 12:
-            return 'Декабрь';
+        return "Январь"
+         case 2:
+        return"Февраль"
+        case 3 :
+        return "Март"
+        case 4 :
+        return "Апрель"
+        case 5 :
+        return "Май"
+        case 6 :
+        return "Июнь"
+        case 7 :
+        return "Июль"
+        case 8 :
+        return "Август"
+        case 9 :
+        return "Сентябрь"
+        case 10 :
+        return "Октябрь"   
+        case 11 :
+        return "Ноябрь "
+        case 12 :
+        return "Декабрь"
+           
         default:
-            return 'Некорректный номер месяца';
+            return 'Некооректный номер месяца'
     }
 }
 
 //5
-
-
- 
-console.log(convertRub(1000, 'AED'));
-
+console.log(convertRub(1000, "USD"));
 function convertRub(valueRub, currency) {
-    switch (currency) {
-        case 'USD':
-            return valueRub / 93.06;
-        case 'EUR':
-            return valueRub / 100.73;
-        case 'KZT':
-            return valueRub / 0.21;
-        case 'AED':
-            return valueRub / 25.34;
-    
-        default:
-            return 'Неизвестная валюта';
-    } 
+   switch (currency) {
+    case "USD":
+        return valueRub/92.75;
+    case "EUR":
+        return valueRub/100.44;
+   case "AED":
+    return valueRub/25.25;
+    case "KZT":
+        return valueRub/20.57;
+    default:
+        return "Неизвестная валюта"
+   } 
 }
