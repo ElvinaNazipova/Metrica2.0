15fcff01851ceb2637eb499258d361009375bea9
Elvina Nazipova <elvina.valeeva.2002@bk.ru>
2024-02-28 23:10:44 +0500

    Выполняю чекпоинт по JS

--- index.js
+++ index.js
@@ -1,49 +1,9 @@
-const person = {
-    //Свойства объекта
-    age: 21,
-    name: 'Elvina',
-    surname: "Nazipova",
-    city: "Surgut",
-    //Методы объекта
-    getFullName: function () {
-        return `${this.name} ${this.surname}`;
-    },
-
-    sayHi: function () {
-        console.log(`Hello, my name is ${this.name}`);
-    },
-
-    isAdult: function () {
-        return this.age >= 18;
-    }
-//До сюда методы
-};
-person.sayHi();
-
-
-console.log(person.getFullName());
-
-console.log(person.isAdult());
-
-
-const people =[
-    {
-    age: 21,
-    name: 'Elvina',
-    surname: "Nazipova",
-    city: "Surgut"
-    },
-    {
-    age: 22,
-    name: 'Rasd',
-    surname: "Poyt",
-    city: "Kipr"
-    }
-]
-
-people.forEach(man => {
-    console.log(man.age);
-
+//Задание 1
+function  printNumbers() {
+   for (let i = -10; i <= 10; i++) {
+    
+    console.log(i);
+   } 
 }
 
-)
+printNumbers();
