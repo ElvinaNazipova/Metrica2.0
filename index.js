cba4735b0b0e56f439509f5dafded2e718a56388
Elvina Nazipova <elvina.valeeva.2002@bk.ru>
2024-02-28 22:47:48 +0500

    Объекты

--- index.js
+++ index.js
@@ -1,6 +1,49 @@
+const person = {
+    //Свойства объекта
+    age: 21,
+    name: 'Elvina',
+    surname: "Nazipova",
+    city: "Surgut",
+    //Методы объекта
+    getFullName: function () {
+        return `${this.name} ${this.surname}`;
+    },
+
+    sayHi: function () {
+        console.log(`Hello, my name is ${this.name}`);
+    },
+
+    isAdult: function () {
+        return this.age >= 18;
+    }
+//До сюда методы
+};
+person.sayHi();
 
 
-for (let i = 1; i <= 10 ; i++) {
-    console.log(i);
-    
-}
+console.log(person.getFullName());
+
+console.log(person.isAdult());
+
+
+// const people =[
+//     {
+//     age: 21,
+//     name: 'Elvina',
+//     surname: "Nazipova",
+//     city: "Surgut"
+//     },
+//     {
+//     age: 22,
+//     name: 'Rasd',
+//     surname: "Poyt",
+//     city: "Kipr"
+//     }
+// ]
+
+// people.forEach(man => {
+//     console.log(man.age);
+
+// }
+
+// )
